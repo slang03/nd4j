4d96d5891d82db3e8a746a3aee9fa04e97323711
eraly <susan.eraly@gmail.com>
2018-03-08 11:49:32 -0800

    clean up

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/ndarray/TestNdArrReadWriteTxt.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/ndarray/TestNdArrReadWriteTxt.java
@@ -1,95 +1,81 @@
-
 package org.nd4j.linalg.api.ndarray;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.nd4j.linalg.BaseNd4jTest;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.factory.Nd4jBackend;
-import org.nd4j.linalg.io.Assert;
-import org.nd4j.linalg.ops.transforms.Transforms;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by susaneraly on 6/18/16.
  */
+@Slf4j
 @RunWith(Parameterized.class)
 public class TestNdArrReadWriteTxt extends BaseNd4jTest {
 
-    public TestNdArrReadWriteTxt(Nd4jBackend backend) {
+    public final static List<INDArray> ARRAYS_TO_TEST_FORDER = new ArrayList<INDArray>() {{
+        //rank - 0
+        add(Nd4j.trueScalar(4)); //[]
+        // rank - 1
+        add(Nd4j.rand('f', new int[]{1}));
+        //rank - 2
+        add(Nd4j.rand('f', new int[]{1, 1}));
+        add(Nd4j.rand('f', new int[]{1, 2}));
+        add(Nd4j.rand('f', new int[]{2, 1}));
+        add(Nd4j.rand('f', new int[]{2, 2}));
+        add(Nd4j.rand('f', new int[]{10, 10}));
+        //rank - 3
+        add(Nd4j.rand('f', new int[]{1, 1, 1}));
+        add(Nd4j.rand('f', new int[]{1, 1, 2}));
+        add(Nd4j.rand('f', new int[]{1, 2, 1}));
+        add(Nd4j.rand('f', new int[]{2, 1, 1}));
+        add(Nd4j.rand('f', new int[]{1, 2, 2}));
+        add(Nd4j.rand('f', new int[]{2, 1, 2}));
+        add(Nd4j.rand('f', new int[]{2, 2, 1}));
+        add(Nd4j.rand('f', new int[]{2, 2, 2}));
+        add(Nd4j.rand('f', new int[]{4, 2, 3}));
+        //rank - 4
+        add(Nd4j.rand('f', new int[]{2, 1, 1, 1}));
+        add(Nd4j.rand('f', new int[]{1, 2, 1, 1}));
+        add(Nd4j.rand('f', new int[]{1, 1, 2, 1}));
+        add(Nd4j.rand('f', new int[]{1, 1, 1, 2}));
+        add(Nd4j.rand('f', new int[]{1, 2, 1, 2}));
+        add(Nd4j.rand('f', new int[]{1, 2, 2, 1}));
+        add(Nd4j.rand('f', new int[]{2, 1, 1, 2}));
+        add(Nd4j.rand('f', new int[]{2, 1, 2, 1}));
+        add(Nd4j.rand('f', new int[]{2, 3, 2, 2}));
+        //rank > 4
+        add(Nd4j.rand('f', new int[]{3, 5, 3, 4, 6}));
+    }};
 
+    public TestNdArrReadWriteTxt(Nd4jBackend backend) {
         super(backend);
     }
 
     @Test
-    public void TestReadWrite() {
-        INDArray origArr = Nd4j.rand('f', 10, 10).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
-        try {
-            Files.delete(Paths.get("someArr.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void TestReadWriteSimple() {
-        INDArray origArr = Nd4j.rand(1, 1).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
-        try {
-            Files.delete(Paths.get("someArr.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void TestReadWriteNd() {
-        INDArray origArr = Nd4j.rand(13, 2, 11, 3, 7, 19).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
-        try {
-            Files.delete(Paths.get("someArr.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void TestWierdShape() {
-        INDArray origArr = Nd4j.rand(1, 1, 2, 1, 1).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
-        try {
-            Files.delete(Paths.get("someArr.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void compareAfterWrite() {
+        for (INDArray origArray : ARRAYS_TO_TEST_FORDER) {
+            log.info("Checking shape ..." + ArrayUtils.toString(origArray.shape()));
+            //log.info(origArray.toString());
+            Nd4j.writeTxt(origArray, "someArr.txt");
+            INDArray readBack = Nd4j.readTxt("someArr.txt");
+            assertEquals("Not equal on shape " + ArrayUtils.toString(origArray.shape()), origArray, readBack);
+            try {
+                Files.delete(Paths.get("someArr.txt"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
